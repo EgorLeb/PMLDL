34e10818cf47e05c05fb7617765d85fc2fcb5e00
Egor Lebedev <e.lebedev@innopolis.university>
2024-09-21 14:04:57 +0300

    work before docker

--- code/deployment/api/main.py
+++ code/deployment/api/main.py
@@ -1,4 +1,53 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi import File, UploadFile, Form
+import numpy as np
+import torch
+import cv2
+import model
 
+
+app = FastAPI()
+
+best_model = model.CNNClassificationModel()
+ckpt = torch.load("best.pt", weights_only=True)
+best_model.load_state_dict(ckpt)
+best_model.eval()
+
+
+def predict(file, predicting_model):
+    arr = np.fromstring(file, 'uint8')
+    im = cv2.imdecode(arr, cv2.COLOR_BGR2RGB)
+
+    # im = cv2.imread(filename)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    res = cv2.resize(im, dsize=(178, 218), interpolation=cv2.INTER_CUBIC)
+
+    res_torch = np.array([res[..., 0], res[..., 1], res[..., 2]])
+    res_torch = np.array(res_torch, dtype='f') / 255
+
+    res_torch = torch.from_numpy(res_torch)
+    p = float(predicting_model(res_torch.unsqueeze(0)))
+    p = 1 if p >= 0 else 0
+    return p
+
+
+@app.get('/')
+def root():
+    return {'message': 'Hello World'}
+
+
+@app.post("/upload")
+def upload(filename: str = Form(...), file: UploadFile = File(...)):
+    res = "Blond"
+    try:
+        contents = file.file.read()
+        if predict(contents, best_model) == 0:
+            res = "Not Blond"
+    except Exception as e:
+        print(e)
+        return {"message": f"{e}"}
+    finally:
+        file.file.close()
+    return {"message": f"{res}"}
 
 
---
+++ code/deployment/app/main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+
+st.title('Blond hair recognition')
+cols = st.columns(2)
+uploaded_file = None
+with cols[0] as col:
+    uploaded_file = st.file_uploader("")
+    if uploaded_file:
+        s = uploaded_file.name.split(".")
+        if len(s) >= 2 and s[-1] in ["jpeg", "png", "jpg"]:
+            bytes_data = uploaded_file.getvalue()
+            st.image(bytes_data, uploaded_file.name)
+with cols[1] as col:
+    if uploaded_file:
+        s = uploaded_file.name.split(".")
+        if len(s) >= 2 and s[-1] in ["jpeg", "png", "jpg"]:
+            bytes_data = uploaded_file.getvalue()
+            url = 'http://127.0.0.1:8000/upload'
+            file = {'file': bytes_data}
+            # resp = requests.post(url=url, json={"file": file, "filename": uploaded_file.name})
+            resp = requests.post(url=url, files=file, data={"filename": uploaded_file.name})
+            st.title(" ")
+            st.title(resp.json()["message"])
+        else:
+            st.title('Choose file with format ("jpeg", "png", "jpg")')
